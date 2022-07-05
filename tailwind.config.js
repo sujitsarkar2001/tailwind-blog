0af3da2e394a7b681016a51a42d8ea3efe0f42aa
Sujit Sarkar <soumyasarkarsujit4@gmail.com>
2022-07-06 03:39:03 +0600

    version 1.0.0

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    
+    extend: {
+      fontFamily: {
+        'poppins': ["Poppins", "sans-serif"],
+        'roboto': ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
